f9eb8459f021eb7f80ca36882677934a2d4a9450
yaojie <2937552332@qq.com>
2026-01-05 18:32:50 +0800

    add llvm

--- examples/example_uvw/src/main.cpp
+++ examples/example_uvw/src/main.cpp
@@ -1,54 +1,23 @@
-#include <memory>
+#include <iostream>
 #include <uvw.hpp>
 
-void listen(uvw::loop &loop) {
-    std::shared_ptr<uvw::tcp_handle> tcp = loop.resource<uvw::tcp_handle>();
-
-    tcp->on<uvw::listen_event>([](const uvw::listen_event &,
-                                  uvw::tcp_handle &srv) {
-        std::shared_ptr<uvw::tcp_handle> client =
-            srv.parent().resource<uvw::tcp_handle>();
-
-        client->on<uvw::close_event>(
-            [ptr = srv.shared_from_this()](
-                const uvw::close_event &, uvw::tcp_handle &) { ptr->close(); });
-        client->on<uvw::end_event>(
-            [](const uvw::end_event &, uvw::tcp_handle &client) {
-                client.close();
-            });
-
-        srv.accept(*client);
-        client->read();
-    });
-
-    tcp->bind("127.0.0.1", 4242);
-    tcp->listen();
-}
-
-void conn(uvw::loop &loop) {
-    auto tcp = loop.resource<uvw::tcp_handle>();
-
-    tcp->on<uvw::error_event>([](const uvw::error_event &,
-                                 uvw::tcp_handle &) { /* handle errors */ });
-
-    tcp->on<uvw::connect_event>(
-        [](const uvw::connect_event &, uvw::tcp_handle &tcp) {
-            auto dataWrite = std::unique_ptr<char[]>(new char[2] { 'b', 'c' });
-            tcp.write(std::move(dataWrite), 2);
-            tcp.close();
-        });
-
-    tcp->connect(std::string { "127.0.0.1" }, 4242);
-}
-
 int main() {
-
+    // 获取默认事件循环
     auto loop = uvw::loop::get_default();
 
-    listen(*loop);
+    // 创建一个定时器资源
+    auto timer = loop->resource<uvw::timer_handle>();
 
-    conn(*loop);
+    // 注册事件监听（在定时器触发时调用）
+    timer->on<uvw::timer_event>(
+        [](const uvw::timer_event &, uvw::timer_handle &) {
+            std::cout << "Timer fired!" << '\n';
+        });
 
+    // 启动定时器：延迟 1000ms（1s）执行，不重复
+    timer->start(std::chrono::milliseconds(1000), std::chrono::milliseconds(0));
+
+    // 运行事件循环
     loop->run();
 
     return 0;
